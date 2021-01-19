833c2e6f345fd9cf042e89da3addc71e5f37875b
Higgins-Codes <30853424+Higgins-Codes@users.noreply.github.com>
2021-01-19 12:24:08 -0800

    Update Screen.js

--- Screen.js
+++ Screen.js
@@ -1,7 +1,16 @@
-const cancel = document.querySelector('reset');
-// Sets the dialog to open
-document.querySelector('dialog').showModal();
+/**
+ * This file is for powering all the events which affect the screen in some way
+ */
 
+// Add button opens dialog box
+const addBtn = document.querySelector('#add');
+addBtn.addEventListener("click", add);
+function add(event) {
+    // Sets the dialog to open
+    document.querySelector('dialog').showModal();
+}
+
+// Cancel button closes dialog box
 const cancelBtn = document.querySelector('#reset');
 cancelBtn.addEventListener("click", reset);
 
