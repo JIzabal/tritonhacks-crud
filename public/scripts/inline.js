7fa7121ab16e3b72cc3b67a2ac5c7743ded6aa3d
Higgins-Codes <30853424+Higgins-Codes@users.noreply.github.com>
2021-02-25 12:48:16 -0800

    Fixed local storage edit

--- public/scripts/inline.js
+++ public/scripts/inline.js
@@ -18,36 +18,6 @@ function deletePostDiv(event) {
     event.parentNode.remove();
 }
 
-
-// Edit
-function editPost(event) {
-    // Edit title
-    // toggle class styling
-    event.parentNode.children[0].classList.toggle('editable');
-    event.parentNode.children[0].toggleAttribute('contenteditable');
-
-    // Edit summary
-    // Get old value
-    let summaryOld = event.parentNode.children[1].textContent;
-
-     
-    let newNode;
-    // Swap tag types
-    console.log(event.parentNode.children[1].tagName);
-    if (event.parentNode.children[1].tagName == 'P') {
-        newNode = document.createElement('textarea');
-    } else {
-        newNode = document.createElement('p');
-    }
-
-    // Ternary operator alternative to the above
-    // let newNode = event.parentNode.children[1].tagName == 'P' ? document.createElement('textarea') : document.createElement('p');
-   
-    event.parentNode.replaceChild(newNode, event.parentNode.children[1]);
-    // Add back in the old text value
-    event.parentNode.children[1].textContent = summaryOld;
-}
-
 // Deletes from local storage
 function deletePost(post) {
     let allPosts = JSON.parse(localStorage.getItem('data'));
@@ -67,6 +37,85 @@ function deletePost(post) {
     }
     console.log("Post delete");
     console.log(allPosts);
+
+    localStorage.setItem('data', JSON.stringify(allPosts));
+}
+
+// Edit
+function editPost(event) {
+    // Edit title
+    // toggle class styling
+    event.parentNode.children[0].classList.toggle('editable');
+    event.parentNode.children[0].toggleAttribute('contenteditable');
+
+    event.parentNode.children[1].classList.toggle('editable');
+    event.parentNode.children[1].toggleAttribute('contenteditable');
+
+    console.log(event.parentNode.children[0]);
+    console.log(event.parentNode.querySelector('h2'));
+    if (!event.parentNode.children[0].isContentEditable) {
+        console.log("false");
+        // Push to local storage
+        let post = {
+            // select the child
+            title: event.parentNode.querySelector('h2').textContent,
+            summary: event.parentNode.querySelector('P').textContent,
+            id: event.parentNode.id
+        };
+        console.log(post);
+        editPostStorage(post);
+    }
+    
+
+    // // Edit summary
+    // // Get old value
+    // let summaryOld = event.parentNode.children[1].textContent;
+
+    // let newNode;
+    // // Swap tag types
+    // console.log(event.parentNode.children[1].tagName);
+    // if (event.parentNode.children[1].tagName == 'P') {
+    //     newNode = document.createElement('textarea');
+    // } else {
+    //     newNode = document.createElement('p');
+    // }
+
+    // // Ternary operator alternative to the above
+    // // let newNode = event.parentNode.children[1].tagName == 'P' ? document.createElement('textarea') : document.createElement('p');
+   
+    // event.parentNode.replaceChild(newNode, event.parentNode.children[1]);
+    // // Add back in the old text value
+    // event.parentNode.children[1].textContent = summaryOld;
+
+    // if (event.parentNode.children[1].tagName == 'textarea') {
+    //     // Push to local storage
+    //     let post = {
+    //         // select the child
+    //         title: event.parentNode.querySelector('h2').value,
+    //         summary: event.parentNode.querySelector('textarea').value,
+    //         id: event.parentNode.id
+    //     };
+    //     editPostStorage(post);
+    // }
+}
+
+function editPostStorage(post) {
+    let allPosts = JSON.parse(localStorage.getItem('data'));
+    console.log(allPosts);
+    allPosts = allPosts !== null ? allPosts : [];
+
+    console.log("Pre-Edit");
+    console.log(allPosts);
+
+    for (i = 0; i < allPosts.length; i++) {
+        if (post.id === allPosts[i].id) {
+            console.log("found post");
+            // Update post values
+            allPosts[i].title = post.title;
+            allPosts[i].summary = post.summary;
+            break;
+        }
+    }
 
     localStorage.setItem('data', JSON.stringify(allPosts));
 }
